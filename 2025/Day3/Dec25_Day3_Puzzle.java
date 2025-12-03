0914cb63ce309f9ea55faa0705e93987b616e457
Anika van Rheede <anika.vanrheede@entelect.co.za>
2025-12-03 06:40:17 +0200

    File path started

--- 2025/Day3/Dec25_Day3_Puzzle.java
+++ 2025/Day3/Dec25_Day3_Puzzle.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Dec25_Day3_Puzzle {
     public static void main(String[] args){
-        
+        String inputFilePath = "./3Dec25_Puzzle1_input.txt";
+
+        try {
+                List<String> inputLines = Files.readAllLines(Paths.get(inputFilePath));
+                
+
+            } catch (IOException e) {
+                System.err.println("Error reading file: " + e.getMessage());
+            }
     }
 }
